66c7b901c8df481d4ef857c416042e6dcd4a1cab
jagmeetRadicle <jsingh@radicleinc.com>
2023-07-20 15:02:52 +0530

    added custom price observer for cart event

---
+++ app/code/Radicle/FirstModule/Observer/CustomPrice.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Radicle\FirstModule\Observer;
+
+use \Magento\Framework\Event\ObserverInterface;
+
+class CustomPrice implements ObserverInterface
+{
+    public function execute(\Magento\Framework\Event\Observer $observer) {
+        $item = $observer->getEvent()->getData('quote_item');
+
+        // Get parent product if current product is child product
+        $item = ( $item->getParentItem() ? $item->getParentItem() : $item );
+
+        //Define your Custom price here
+        $price = 100;
+
+        //Set custom price
+        $item->setCustomPrice($price);
+        $item->setOriginalCustomPrice($price);
+        $item->getProduct()->setIsSuperMode(true);
+    }
+}
